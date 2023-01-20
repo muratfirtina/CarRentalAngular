b8ad625ef28cdcb5987b10238bfaab48f1ea844a
muratfirtina <muratfirtina@hotmail.com>
2023-01-20 23:54:54 +0300

    20.01.23 fe1

--- CarRentalAngular/src/app/components/car/car.component.ts
+++ CarRentalAngular/src/app/components/car/car.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
-car1:any ={carId:1,brandId:1,colorId:1,modelYear:2010,dailyPrice:100,description:"Audi A3"};
-car2:any ={carId:2,brandId:1,colorId:2,modelYear:2011,dailyPrice:200,description:"Audi A4"};
-car3:any ={carId:3,brandId:1,colorId:3,modelYear:2012,dailyPrice:300,description:"Audi A5"};
+export class CarComponent implements OnInit {
+  cars:Car[] = [];
+  dataLoaded = false;
 
-cars=[this.car1,this.car2,this.car3];
+  constructor(private carService:CarService) { }
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+  getCars(){
+    this.carService.getCars().subscribe(response => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    })
+  }
 }
